8f1407bc7bf74e218ca6174e314aeb5524a26a64
KamelSenfro <kamel.fares02@eng-st.cu.edu.eg>
2023-07-27 12:06:11 +0300

    fixing big data errors

--- function translateColumnDToEnglish() {.js
+++ function translateColumnDToEnglish() {.js
@@ -1,19 +1,24 @@
 function translateColumnDToEnglish() {
     var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-    var sourceRange = sheet.getRange("D2:D225001"); // Specify the range containing the source text (Arabic) you want to translate
-    var targetRange = sheet.getRange("I2:I225001"); // Specify the target range where the translated text (English) will be written
+     // Specify the range containing the source text (Arabic) you want to translate
+    var sourceRange = sheet.getRange("D2:D225001");
+    // Specify the target range where the translated text (English) will be written
+    var targetRange = sheet.getRange("I2:I225001"); 
   
     var sourceValues = sourceRange.getValues();
     var translatedValues = [];
-    var translator = LanguageApp.getService().translate(); // Create a Translator object to use the LanguageApp service
   
     for (var i = 0; i < sourceValues.length; i++) {
       var sourceText = sourceValues[i][0];
-      var translatedText = translator.translate(sourceText, "ar", "en"); // Translate from Arabic (ar) to English (en)
-      translatedValues.push([translatedText]); // Store the translated text in the array
-      Utilities.sleep(1000); // Add a delay of 1000 milliseconds (1 second) between translation calls to avoid throttling
+
+      // Translate from Arabic (ar) to English (en)
+      var translatedText = LanguageApp.translate(sourceText, "ar", "en"); 
+      // Store the translated text in the array
+      translatedValues.push([translatedText]); 
+      // Add a delay of 1000 milliseconds (1 second) between translation calls to avoid throttling
+      Utilities.sleep(1000); 
     }
-  
-    targetRange.setValues(translatedValues); // Write the translated text to the target range (column I)
+  // Write the translated text to the target range (column I)
+    targetRange.setValues(translatedValues); 
   }
   
